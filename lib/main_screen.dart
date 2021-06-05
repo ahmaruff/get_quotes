21ea6519ce47056c2a59dd786e78ce7c385c8813
ahmaruff <ahmadmaruf2701@gmail.com>
2021-06-05 22:27:59 +0700

    reload API button done. display other buttons but no functionality yet

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,10 +1,9 @@
-//import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:get_quotes/quote_model.dart';
 import 'package:get_quotes/quote_service.dart';
+import 'package:get_quotes/fav_button.dart';
 
 class MainScreen extends StatefulWidget {
   MainScreen({Key? key}) : super(key: key);
@@ -22,6 +21,12 @@ class _MainScreenState extends State<MainScreen> {
     futureQuoteModel = getQuote();
   }
 
+  // @override
+  // void didChangeDependencies(){
+  //   super.didChangeDependencies()
+
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +34,7 @@ class _MainScreenState extends State<MainScreen> {
         child: Column(
           children: <Widget>[
             Container(
-              margin: EdgeInsets.only(top: 30, bottom: 20),
+              margin: EdgeInsets.only(top: 30, bottom: 30),
               child: Center(
                 //heightFactor: 3.0,
                 child: Text(
@@ -45,85 +50,137 @@ class _MainScreenState extends State<MainScreen> {
             FutureBuilder<QuoteModel>(
               future: futureQuoteModel,
               builder: (context, snapshoot) {
-                if (snapshoot.hasData) {
-                  final quote = snapshoot.data!;
-                  return Expanded(
-                    child: Container(
-                      margin: EdgeInsets.symmetric(
-                          horizontal: 30.0, vertical: 10.0),
-                      child: ListView(
-                        children: <Widget>[
-                          Center(
-                            child: Text(
-                              '${quote.content}',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontFamily: 'Dancing Script',
-                                fontSize: 20.0,
-                                //fontSize: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+                if (snapshoot.connectionState == ConnectionState.done) {
+                  if (snapshoot.hasData) {
+                    var quoteModel = snapshoot.data!;
+                    final quote = quoteModel;
+                    return Expanded(
+                      child: Container(
+                        width: 250,
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 30.0, vertical: 10.0),
+                        child: ListView(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Center(
+                              child: Text(
+                                '${quote.content}',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontFamily: 'Dancing Script',
+                                  fontSize: 20.0,
+                                  //fontSize: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+                                ),
                               ),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Center(
+                              child: Text(
+                                '~ ${quote.author} ~',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  } else {
+                    return Container(
+                      margin: EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 20.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            'Oops!',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Berkshire Swash',
+                              fontSize: 40,
+                              //fontSize: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 4),
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                           SizedBox(
-                            height: 30,
+                            height: 24.0,
                           ),
-                          Center(
-                            child: Text(
-                              '~ ${quote.author} ~',
-                              style: TextStyle(fontWeight: FontWeight.bold),
+                          Text(
+                            'Please Check Your Connection or Try Again Later',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black38,
                             ),
                           ),
                         ],
                       ),
-                    ),
-                  );
-                } else if (snapshoot.hasError) {
-                  return Container(
-                    margin:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          'Oops!',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontFamily: 'Berkshire Swash',
-                            fontSize: 40,
-                            //fontSize: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 4),
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 24.0,
-                        ),
-                        Text(
-                          'Please Check Your Connection or Try Again Later',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.black38,
-                          ),
-                        ),
-                      ],
-                    ),
+                    );
+                  }
+                } else {
+                  return Center(
+                    heightFactor: 2.0,
+                    child: CircularProgressIndicator(),
                   );
                 }
-                return Center(
-                  heightFactor: 2.0,
-                  child: CircularProgressIndicator(),
-                );
               },
             ),
           ],
         ),
       ),
-      // floatingActionButton: new FloatingActionButton(
-      //   onPressed: setState(() {
-      //     futureQuoteModel = getQuote();
-      //   }),
-      //   child: const Icon(Icons.refresh),
-      // ),
+      bottomNavigationBar: Container(
+        height: 64,
+        width: 250,
+        margin: EdgeInsets.symmetric(vertical: 24, horizontal: 12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.refresh),
+                  color: Colors.purple,
+                  onPressed: () {
+                    setState(() {
+                      futureQuoteModel = getQuote();
+                    });
+                  },
+                ),
+                Text(
+                  'Refresh',
+                  style: TextStyle(color: Colors.purple),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                FavoriteButton(),
+                Text(
+                  'Favorite',
+                  style: TextStyle(color: Colors.purple),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.bookmark_border),
+                  color: Colors.purple,
+                  onPressed: () {},
+                ),
+                Text(
+                  'Collection',
+                  style: TextStyle(color: Colors.purple),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
